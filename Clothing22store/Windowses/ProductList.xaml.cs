3806da61bde8b0bab5bb176a26f8cdadd94a1769
IMAC <tysim190@gmail.com>
2023-02-09 12:20:25 +0300

    2265

---
+++ Clothing22store/Windowses/ProductList.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Clothing22store.Db;
+using Clothing22store.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Clothing22store.Windowses
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductList.xaml
+    /// </summary>
+    public partial class ProductList : Window
+    {
+        public ProductList()
+        {
+            InitializeComponent();
+            GetListProduct();
+        }
+
+        
+    }
+
+    private void GetListProduct()
+    {
+        List<Product> products = new List<Product>();
+        products = EFClass.Context.Product.ToList();
+
+        LvProduct.ItemsSource = products;
+    }
+}
+}
+    }
+}
--- Clothing22store/Windowses/registration.xaml.cs
+++ Clothing22store/Windowses/registration.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Clothing22store.Db;
 using Clothing22store.EF;
 
 namespace Clothing22store.Windowses
@@ -25,6 +26,41 @@ namespace Clothing22store.Windowses
             InitializeComponent();
             CmbGender.ItemsSource = EF.EfClass.Context.Gender.ToList();
             CmbGender.SelectedIndex = 0;
+            CmbGender.DisplayMemberPath = "GenderName";
+        }
+
+        private void BtnAdduser_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (string.IsNullOrWhiteSpace(TbLogin.Text))
+            {
+                MessageBox.Show("Логин не может быть пустым или состоять из пробелов");
+                return;
+            }
+
+           
+            EF.EfClass.Context.User.Add(new User()
+            {
+                Login = TbLogin.Text,
+                Password = PbPassword.Password,
+                LastName = TblName.Text,
+                FirstName = TbFName.Text,
+                Patronymic = TbPatronumic.Text,
+                Email = TbEmail.Text,
+                PhoneNumber = TbPhone.Text,
+                Birthday = DpDateOfBirth.SelectedDate.Value,
+                IDGender = (CmbGender.SelectedItem as Gender).IDGender,
+
+            });
+
+           
+            EF.EfClass.Context.SaveChanges();
+
+
+
+            MessageBox.Show("Ok");
+
+
 
 
         }
